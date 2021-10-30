83e3dc0ea57fa25788c01e871069361157416966
Richard Lyon <richlyon@mac.com>
2021-10-30 05:14:54 +0100

    publish/display feature
    Move libraries to github

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <secrets.h>
 
 #define SENSOR_TYPE "energy"
-#define FIRMWARE_VERSION "1.1.0"
+#define FIRMWARE_VERSION "2.0.0"
 #define BOILER_SENSOR_PIN 27
 
 // OLED pins
@@ -16,13 +16,12 @@
 // Network credentials
 // #define WIFI_SSID xxx
 // #define WIFI_PASSWORD xxx
-#define MQTT_HOST IPAddress(192, 168, 1, 1) // Mac Mini M1
+#define MQTT_HOST IPAddress(192, 168, 1, 240) // Mac Mini M1
 #define MQTT_PORT 1883
+#define TOPIC_ROOT "kw_sensors"
 
-// MQTT topics
-char topicDataBoilerStateLED[MAX_TOPIC_BUFFER_LEN];
-char topicDataBoilerCumulativeLED[MAX_TOPIC_BUFFER_LEN];
-char topicCommandResetTimer[MAX_TOPIC_BUFFER_LEN];
+uint8_t stateField;
+uint8_t cumulativeSecsField;
 
 kwHeltecWifikit32 heltec;
 kwBoiler boiler{ BOILER_SENSOR_PIN };
@@ -32,51 +31,33 @@ void setup()
 {
     Serial.begin(115200);
     heltec.initDisplay(PIN_RST, PIN_SDA, PIN_SCL, true);
-    // heltec.initNetwork(WIFI_SSID, WIFI_PASSWORD, MQTT_HOST);
 
-    heltec.makeTopic("data", "boilerState", "LED", topicDataBoilerStateLED);
-    heltec.makeTopic("data", "boilerCumulativeSecs", "LED", topicDataBoilerCumulativeLED);
-    heltec.makeTopic("command", "resetTimer", "LED", topicCommandResetTimer);
-    
-    Serial.printf("------------------%s sensor------------------\n", SENSOR_TYPE);
-    Serial.printf("Firmware              : %s\n", FIRMWARE_VERSION);
-    Serial.printf("Device ID             : %s\n", heltec.deviceID);
-    Serial.printf("Sensor pin            : %d\n", boiler.sensorPin);
-    Serial.printf("MQTT BoilerState      : %s\n", topicDataBoilerStateLED);
-    Serial.printf("MQTT BoilerCumulative : %s\n", topicDataBoilerCumulativeLED);
-    Serial.printf("MQTT Reset Timer      : %s\n", topicCommandResetTimer);
-    Serial.printf("MQTT Status           : %s\n", heltec.topicMetaStatus);
+    stateField = heltec.registerDataTopic("State", "degC", "boilerState", "LED");
+    cumulativeSecsField = heltec.registerDataTopic("On time", "secs", "boilerCumulativeSecs", "LED");
+
+    Serial.printf("\n------------------%s sensor------------------------------------------------\n\n", SENSOR_TYPE);
+    Serial.printf("%-12s : %s\n", "Firmware", FIRMWARE_VERSION);
+    Serial.printf("%-12s : %s\n", "Device ID", heltec.deviceID);
+    for (int i=0; i < heltec.dataTopics.size(); i++)
+    {
+      Serial.printf("%-12s : %s\n", heltec.dataTopics[i].fieldName.c_str(), heltec.dataTopics[i].topicString.c_str());
+    }
+    Serial.printf("%-12s : %s\n", "status", heltec.metaTopics[heltec.statusTopicID].c_str());
+  
+    heltec.initWiFi(WIFI_SSID, WIFI_PASSWORD);
+    heltec.initMTTQ(MQTT_HOST, TOPIC_ROOT);
     
     publishDataTimer.set(1000);
-
-    delay(1000);
   }
 
 void loop() {
   
   if (publishDataTimer.repeat())
   {
-    char buf[20];
-    
-    // boiler state
-    uint16_t boilerState = boiler.readState();
-    heltec.publish(topicDataBoilerStateLED, boilerState);
-    heltec.display((boilerState == 1) ? "HEATING" : "OFF", 0);
-    
-    // cumulative time
-    uint16_t cumulativeSeconds = boiler.cumulativeSeconds();
-    heltec.publish(topicDataBoilerCumulativeLED, cumulativeSeconds);
+      heltec.publish(stateField, boiler.readState());
+      heltec.publish(cumulativeSecsField, boiler.cumulativeSeconds());
 
-    if (cumulativeSeconds >= 60)
-    {
-      uint16_t cumulativeMinutes = cumulativeSeconds / 60;
-      sprintf(buf, "%d minute%s", cumulativeMinutes, (cumulativeMinutes > 0) ? "s" : "");
-    }
-    else{
-      sprintf(buf, "%d seconds", cumulativeSeconds);
-    }
-    
-    heltec.display(buf, 6);
+      heltec.display();
   }
   
   heltec.run();
