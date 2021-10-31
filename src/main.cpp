40a3938ab5698d8b93f6037f459a28fbf84f44e9
Richard <richlyon@mac.com>
2021-10-31 16:43:05 +0000

    Refactor to use kwHeltecWifiKit32 1.0.3

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,8 @@
 #include <secrets.h>
 
 #define SENSOR_TYPE "energy"
-#define FIRMWARE_VERSION "2.0.0"
+#define FIRMWARE_VERSION "2.1.0"
 #define BOILER_SENSOR_PIN 27
-
-// OLED pins
-#define PIN_RST 16
-#define PIN_SDA 4
-#define PIN_SCL 15
-
-// Network credentials
-// #define WIFI_SSID xxx
-// #define WIFI_PASSWORD xxx
-#define MQTT_HOST IPAddress(192, 168, 1, 240) // Mac Mini M1
-#define MQTT_PORT 1883
-#define TOPIC_ROOT "kw_sensors"
 
 uint8_t stateField;
 uint8_t cumulativeSecsField;
@@ -27,28 +15,35 @@ kwHeltecWifikit32 heltec;
 kwBoiler boiler{ BOILER_SENSOR_PIN };
 kwNeoTimer publishDataTimer = kwNeoTimer();
 
+HeltecConfig config;
+
 void setup() 
 {
     Serial.begin(115200);
-    heltec.initDisplay(PIN_RST, PIN_SDA, PIN_SCL, true);
 
-    stateField = heltec.registerDataTopic("State", "", "boilerState", "LED");
-    cumulativeSecsField = heltec.registerDataTopic("On time", "secs", "boilerCumulativeSecs", "LED");
+    config.firmwareVersion = FIRMWARE_VERSION;
+    config.ssid = WIFI_SSID;
+    config.pwd = WIFI_PASSWORD;
+    config.mqtt_host = IPAddress(192, 168, 1, 240); // Mac Mini M1
+    config.topicRoot = "kw_sensors";
+    config.rotateDisplay = true;
+
+    stateField = heltec.registerField("State", "", "boilerState", "LED");
+    cumulativeSecsField = heltec.registerField("On time", "secs", "boilerCumulativeSecs", "LED");
+    
+    publishDataTimer.set(1000);
+    
+    heltec.init(config);
 
     Serial.printf("\n------------------%s sensor------------------------------------------------\n\n", SENSOR_TYPE);
     Serial.printf("%-12s : %s\n", "Firmware", FIRMWARE_VERSION);
     Serial.printf("%-12s : %s\n", "Device ID", heltec.deviceID);
+    Serial.printf("%-12s : %s %s\n", "RTC", heltec.hasRTC ? "OK" : "Not found", heltec.rtcWasAdjusted ? "[was not running - check time]" : "");
+    Serial.printf("%-12s : %s\n", "Status", heltec.metaTopics[heltec.statusTopicID].c_str());
     for (int i=0; i < heltec.dataTopics.size(); i++)
     {
       Serial.printf("%-12s : %s\n", heltec.dataTopics[i].fieldName.c_str(), heltec.dataTopics[i].topicString.c_str());
     }
-    Serial.printf("%-12s : %s\n", "status", heltec.metaTopics[heltec.statusTopicID].c_str());
-  
-    heltec.initWiFi(WIFI_SSID, WIFI_PASSWORD);
-    heltec.initMTTQ(MQTT_HOST, TOPIC_ROOT);
-    heltec.initTimeSync();
-
-    publishDataTimer.set(1000);
   }
 
 int i = 0;
@@ -60,8 +55,6 @@ void loop()
 
       heltec.publish(stateField, boiler.readState());
       heltec.publish(cumulativeSecsField, boiler.activeSeconds());
-
-      heltec.display();
   }
   heltec.run();
 }
